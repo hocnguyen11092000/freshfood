0ad025ae6a9b1d295dadc7b184337016bfd246c2
Hoc Nguyen <hocnguyen1109200@gmail.com>
2022-03-26 19:06:41 +0700

    basic responsive

--- src/features/client/blog/Blog.tsx
+++ src/features/client/blog/Blog.tsx
@@ -1,9 +1,31 @@
-import React from "react";
+import Footer from "components/Common/footer/Footer";
+import Header from "components/Common/header/Header";
+import SidebarMobile from "components/Common/sidebarMobile/SidebarMobile";
+import React, { useState } from "react";
 import "./blog.scss";
 type Props = {};
 
 const Blog = (props: Props) => {
-  return <div className="blog">updating...</div>;
+  const [showMobile, setShowMobile] = useState<number>(300);
+
+  const handleChangeMobileSidebar = () => {
+    setShowMobile(0);
+  };
+
+  const handleCloseMobileSidebar = () => {
+    setShowMobile(300);
+  };
+  return (
+    <>
+      <Header onChange={handleChangeMobileSidebar}></Header>
+      <div className="blog">updating...</div>
+      <Footer bg="#f0f0f0"></Footer>
+      <SidebarMobile
+        onClose={handleCloseMobileSidebar}
+        show={showMobile}
+      ></SidebarMobile>
+    </>
+  );
 };
 
 export default Blog;
