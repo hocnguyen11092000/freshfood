7890031cbe6c89130ee9cdb0a20b6c5863ec2ffe
Hoc Nguyen <hocnguyen1109200@gmail.com>
2022-03-26 18:59:48 +0700

    basic responsive

--- src/components/Layout/user/User.tsx
+++ src/components/Layout/user/User.tsx
@@ -5,6 +5,7 @@ import Contact from "features/client/contact/Contact";
 import Home from "features/client/Home/Home";
 import DetailProduct from "features/client/product/pages/detailProduct/DetailProduct";
 import ListProduct from "features/client/product/pages/listProduct/ListProduct";
+import Blog from "features/client/blog/Blog";
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 
@@ -25,6 +26,8 @@ const UserLayout = (props: Props) => {
         <Route path="/shop" element={<ListProduct></ListProduct>}></Route>
         {/* contact */}
         <Route path="/contact" element={<Contact></Contact>}></Route>
+        {/* blog */}
+        <Route path="/blog" element={<Blog></Blog>}></Route>
         {/* checkout */}
         <Route path="/checkout" element={<CheckOut></CheckOut>}></Route>
         <Route
---
+++ src/features/client/blog/Blog.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import "./blog.scss";
+type Props = {};
+
+const Blog = (props: Props) => {
+  return <div className="blog">updating...</div>;
+};
+
+export default Blog;
